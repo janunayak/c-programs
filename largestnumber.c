b4e17c6586bbe1f994e559792c58516216214049
janunayak <47571647+janunayak@users.noreply.github.com>
2019-09-08 10:08:23 +0530

    Add files via upload

---
+++ largestnumber.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	printf("\nEnter the three numbers: ");
+	scanf("%d%d%d",&a,&b,&c);
+	if(a>b)
+	if(a>c)
+	printf("%d",a);
+	else
+	printf("%d",c);
+	else
+	if(b>c)
+	printf("%d",b);
+	else
+	printf("%d",c);
+}
